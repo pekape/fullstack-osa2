83eaeeba3128bb002526e77c1cec1fc01dd73b9c
pekape <37801685+pekape@users.noreply.github.com>
2018-03-30 11:25:36 +0300

    2.4

--- kurssi/src/index.js
+++ kurssi/src/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Otsikko = ({nimi}) => <h1>{nimi}</h1>
+const Otsikko = ({nimi}) => <h2>{nimi}</h2>
 
 const Osa = ({osa}) => <p>{osa.nimi} {osa.tehtavia}</p>
 
 const Sisalto = ({osat}) => {
-  const osatKomponentteina = osat.map(osa => <Osa key={osa.nimi} osa={osa} />)
+  const osaKomponentit =
+    osat.map(osa => <Osa key={osa.id} osa={osa} />)
+
   return (
     <div>
-      {osatKomponentteina}
+      {osaKomponentit}
     </div>
   )
 }
@@ -35,27 +37,53 @@ const Kurssi = ({kurssi}) => (
 )
 
 const App = () => {
-  const kurssi = {
-    nimi: 'Half Stack -sovelluskehitys',
-    osat: [
-      {
-        nimi: 'Reactin perusteet',
-        tehtavia: 10
-      },
-      {
-        nimi: 'Tiedonvälitys propseilla',
-        tehtavia: 7
-      },
-      {
-        nimi: 'Komponenttien tila',
-        tehtavia: 14
-      }
-    ]
-  }
+  const kurssit = [
+    {
+      nimi: 'Half Stack -sovelluskehitys',
+      id: 1,
+      osat: [
+        {
+          nimi: 'Reactin perusteet',
+          tehtavia: 10,
+          id: 1
+        },
+        {
+          nimi: 'Tiedonvälitys propseilla',
+          tehtavia: 7,
+          id: 2
+        },
+        {
+          nimi: 'Komponenttien tila',
+          tehtavia: 14,
+          id: 3
+        }
+      ]
+    },
+    {
+      nimi: 'Node.js',
+      id: 2,
+      osat: [
+        {
+          nimi: 'Routing',
+          tehtavia: 3,
+          id: 1
+        },
+        {
+          nimi: 'Middlewaret',
+          tehtavia: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  const kurssiKomponentit =
+    kurssit.map(kurssi => <Kurssi key={kurssi.id} kurssi={kurssi} />)
 
   return (
     <div>
-      <Kurssi kurssi={kurssi} />
+      <h1>Opetusohjelma</h1>
+      {kurssiKomponentit}
     </div>
   )
 }
